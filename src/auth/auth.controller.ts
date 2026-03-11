739070c5b67ea7e756aa7328d8d045e41863bba5
Shannnnn <sacanoy16@gmail.com>
2026-03-11 18:54:36 +0800

    feat: add auth module with DTO validation using class-validator

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthDto } from './authDto';
+
+@Controller('auth')
+export class AuthController {
+    @Post('register')
+    @UsePipes(ValidationPipe)
+    registerUser(@Body() userData: AuthDto){
+        return {
+            Email: `${userData.email}`,
+        };
+    }
+}
